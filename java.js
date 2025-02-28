fc1ec86826c2443ae884eef7c0f2be8e8889e18b
DanGoj <danikor2013@gmail.com>
2025-02-28 22:21:34 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,28 @@
+const canvas = document.querySelector("#confetti")
+
+const jsConfetti = new JSConfetti();
+
+
+
+const date = new Date();
+if (date.getDay() >=5) {
+	svar="JA!"
+	jsConfetti.addConfetti({
+	emojis: ['🍾','🍻','🥂','🍸','🍺','🍷','🍹', "JA"],
+	//confettiRadius: 100,
+	//confettiNumber: 50,
+	emojiSize: 120, 
+})
+	jsConfetti.addConfetti({
+	confettiRadius: 6,
+})
+} else {
+	svar="Nej"
+	jsConfetti.addConfetti({
+	emojis: ['NEJ', ''],
+	confettiRadius: 100,
+	//confettiNumber: 50,
+	emojiSize: 120,
+})
+}
+document.getElementById("svar").innerHTML = svar;
